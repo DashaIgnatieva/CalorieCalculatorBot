b2fe3129211d9fcc9b8e219ee8827f57b1740bd5
DashaIgnatieva <dariyignatieva@mail.ru>
2023-05-23 18:05:43 +0300

    added a counting code for calories in main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Здесь скрипт для работы с пользователем, для получения от него данных о продукте
 
 from formulas import product_calorie
+from queryset import Queryset
 import re
 
 row_data = str(input('Введите название и вес продукта в граммах через запятую: ')) # Получаем данные в формате строки от пользователя
@@ -13,10 +14,16 @@ while True:
     else:
         row_data = str(input('Вы неверно ввели данные. Введите название продукта и его вес в граммах через запятую: ')).lower()
 
-data_for_db = row_data.split(',')
+data_for_db = row_data.split(',') # разбиваем строку по запятой
 
-for i in range(len(data_for_db)):
-    data_for_db[i] = data_for_db[i].strip()
+for i in range(len(data_for_db)): 
+    data_for_db[i] = data_for_db[i].strip() # удаляем пробелы в начале и конце каждого элемента
 
-print(data_for_db)
+user_product_weight = re.match(r'^\d+', data_for_db[1]) # отделяем цыфры от букв, если они есть
 
+weight_for_count = int(user_product_weight.group()) # Сщхраняем вес пордукта без всего лишнего с типом int
+cal_100gr = Queryset.find_calories_by_product_name(data_for_db[0]) # калорийность продукта на 100 гр из БД
+
+user_product_calorie = product_calorie(cal_100gr, weight_for_count) # Передаем в функцию для подсчета калорий продукта все необходимое
+
+print(f'Калорийность вашего продукта: {user_product_calorie} ккал') # Выводим пользователю калорийность его продукта
--- queryset.py
+++ queryset.py
@@ -1,5 +1,13 @@
-# Заглушка для теста получения данных от пользователя. Удалю когда подключу БД
+# Класс для работы с бд
+import sqlite3
 
-class product():
-    cal_product = 69
+class Queryset():
     
+    def __init__(self):
+        pass
+
+    def find_calories_by_product_name(product_name):
+        return 35
+
+    def product_calorie():
+        pass
