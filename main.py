3e5ba6ac56f864394e1c7a1ce743c2b8d7279ec6
DashaIgnatieva <dariyignatieva@mail.ru>
2023-05-30 16:54:49 +0300

    Connected venv and added requirenments.txt

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 # Здесь скрипт для работы с пользователем, для получения от него данных о продукте
 
 from formulas import product_calorie
-from queryset import Queryset
+from queryset import *
 import re
 
-row_data = str(input('Введите название и вес продукта в граммах через запятую: ')) # Получаем данные в формате строки от пользователя
-row_data.lower() # приводим все буквы к нижнему регистру
+row_data = str(input('Введите название и вес продукта в граммах через запятую: '))  # Получаем данные в формате строки от пользователя
+row_data.lower()
 
 pattern = r'^\s*[А-Яа-яЁё ]+\s*,\s*\d+\s*(грамма|грамм|гр)?$' # регулярка для проверки строки вписанной пользователем
 while True:
@@ -21,9 +21,10 @@ for i in range(len(data_for_db)):
 
 user_product_weight = re.match(r'^\d+', data_for_db[1]) # отделяем цыфры от букв, если они есть
 
-weight_for_count = int(user_product_weight.group()) # Сщхраняем вес пордукта без всего лишнего с типом int
-cal_100gr = Queryset.find_calories_by_product_name(data_for_db[0]) # калорийность продукта на 100 гр из БД
+weight_for_count = int(user_product_weight.group()) # Сохраняем вес продукта без всего лишнего с типом int
+
+cal_100gr = get_calories(data_for_db[0].lower())
 
 user_product_calorie = product_calorie(cal_100gr, weight_for_count) # Передаем в функцию для подсчета калорий продукта все необходимое
 
-print(f'Калорийность вашего продукта: {user_product_calorie} ккал') # Выводим пользователю калорийность его продукта
+print(f'Калорийность {weight_for_count} гр вашего продукта: {user_product_calorie} ккал') # Выводим пользователю калорийность его продукта
